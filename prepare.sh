837810b8caf8084afa593fd2448813e46a55b61f
Mattia Dongili <malattia@linux.it>
2005-08-07 10:42:44 +0000

    removing reduntant files

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,11 @@
 
 #!/bin/bash
-WANT_AUTOMAKE="1.8" aclocal && libtoolize -c -f && chmod +x ltmain.sh && \
+WANT_AUTOMAKE="1.8" aclocal && libtoolize -c -f && \
 aclocal && autoconf && automake -a -c --foreign && \
 ( \
 	echo "Entering src"
 	cd src &&
 	WANT_AUTOMAKE="1.8" aclocal && \
-		libtoolize -c -f && \
 		autoconf && \
 		autoheader && \
 		automake -a -c --foreign
@@ -16,4 +15,6 @@ aclocal && autoconf && automake -a -c --foreign && \
 	cd manpages &&
 	WANT_AUTOMAKE="1.8" aclocal && autoconf && automake -a -c --foreign
 	cd ..
-)
+) && chmod +x ltmain.sh
+
+		#libtoolize -c -f && \
--- src/config_parser.c
+++ src/config_parser.c
@@ -207,11 +207,25 @@ int parse_config_general (FILE *config) {
 			continue;
 		}
 
-		if (strcmp(name,"acpi_workaround") == 0) {
+		if (strcmp(name,"socketfile") == 0) {
 			if (value != NULL) {
-				configuration.acpi_workaround = atoi (value);
+				strncpy(configuration.sockfile, value, MAX_PATH_LEN);
+			} else {
+				cpufreqd_log(LOG_WARNING, 
+						"parse_config_general(): empty \"socketfile\", "
+						"using default %s.\n", CPUFREQD_SOCKFILE);
+				strncpy(configuration.pidfile, CPUFREQD_SOCKFILE, MAX_PATH_LEN);
+			}
+			configuration.sockfile[MAX_PATH_LEN-1] = '\0';
+			continue;
+		}
+
+		if (strcmp(name,"enable_remote") == 0) {
+			if (value != NULL) {
+				configuration.enable_remote = atoi (value);
 				cpufreqd_log(LOG_WARNING, "parse_config_general(): "
-						"ACPI workaround enabled.\n");
+						"Remote control %s.\n", 
+						configuration.enable_remote ? "enabled" : "disabled");
 			}
 			continue;
 		}
