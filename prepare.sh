eab2ffa816db617527863e619cf77d0187249e1a
Mattia Dongili <malattia@linux.it>
2005-09-25 16:58:40 +0000

    simplified autothings. Now use a single configure script

--- prepare.sh
+++ prepare.sh
@@ -2,32 +2,33 @@
 #!/bin/bash
 WANT_AUTOMAKE="1.8" aclocal && libtoolize -c -f && \
 aclocal && autoconf && automake -a -c --foreign \
-&& ( \
-	ENTER_INTO=src
-	echo "Entering $ENTER_INTO"
-	cd $ENTER_INTO
-	WANT_AUTOMAKE="1.8" aclocal && \
-		autoconf && \
-		autoheader && \
-		automake -a -c --foreign
-	cd ..
-) && ( \
-	ENTER_INTO=utils
-	echo "Entering $ENTER_INTO"
-	cd $ENTER_INTO
-	WANT_AUTOMAKE="1.8" aclocal && \
-		autoconf && \
-		autoheader && \
-		automake -a -c --foreign
-	cd ..
-) && ( \
-	ENTER_INTO=manpages
-	echo "Entering $ENTER_INTO"
-	cd $ENTER_INTO
-	WANT_AUTOMAKE="1.8" aclocal && \
-		autoconf && \
-		automake -a -c --foreign
-	cd ..
-) && chmod +x ltmain.sh
+&& chmod +x ltmain.sh
+#&& ( \
+#	ENTER_INTO=src
+#	echo "Entering $ENTER_INTO"
+#	cd $ENTER_INTO
+#	WANT_AUTOMAKE="1.8" aclocal && \
+#		autoconf && \
+#		autoheader && \
+#		automake -a -c --foreign
+#	cd ..
+#) && ( \
+#	ENTER_INTO=utils
+#	echo "Entering $ENTER_INTO"
+#	cd $ENTER_INTO
+#	WANT_AUTOMAKE="1.8" aclocal && \
+#		autoconf && \
+#		autoheader && \
+#		automake -a -c --foreign
+#	cd ..
+#) && ( \
+#	ENTER_INTO=manpages
+#	echo "Entering $ENTER_INTO"
+#	cd $ENTER_INTO
+#	WANT_AUTOMAKE="1.8" aclocal && \
+#		autoconf && \
+#		automake -a -c --foreign
+#	cd ..
+#) \
 
 		#libtoolize -c -f && \
