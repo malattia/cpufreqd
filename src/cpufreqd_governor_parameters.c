ce368adae90e2767c639c04733a77982d689ffac
Mattia Dongili <malattia@linux.it>
2006-04-23 18:46:47 +0000

    updated governor_parameter plugin to build with libsysfs2. Thanks Heiko Noordhof

--- src/cpufreqd_governor_parameters.c
+++ src/cpufreqd_governor_parameters.c
@@ -18,7 +18,7 @@
 
 /*
  * Governor parameter plugin for cpufreqd
- * Version 0.2
+ * Version 0.3
  */
 
 #include <stdlib.h>
@@ -30,16 +30,18 @@
 
 #include "cpufreqd_plugin.h"
 
-#define MAXLEN_PARAMETER_VALUE 24
-#define SYS_CPU_ROOT "system/cpu"
+#define BUFLEN_PARAMETER_VALUE 24
+#define SYS_CPU_DIR "devices/system/cpu"
 
 static unsigned int number_of_cpus = 1;
+static char syspath_cpu[SYSFS_PATH_MAX];
 
 static const char *supported_governors[] = {
 	"ondemand",
 	"conservative",
 	NULL
 };
+
 
 /* Governor parameter object struct.
  */
@@ -50,76 +52,38 @@ struct gov_parameter {
 };
 
 
-/* Gets sysfs attribute for a governor parameter
- * Return pointer to the struct if successfull, NULL on error.
+/* Opens a sysfs device for the cpufreq governor of a given CPU number.
+ * Returns a pointer to the device if successfull, NULL on error.
  */
-static struct sysfs_attribute *get_governor_attribute(struct sysfs_root_device *cpufreqdev,
-		const char *governor, 
-		const char *parameter)
+static struct sysfs_device *open_governor_device(unsigned int cpu, const char *governor)
 {
+	char govdev_path[SYSFS_PATH_MAX];
 	struct sysfs_device *govdev = NULL;
-	struct sysfs_attribute *govattr = NULL;
-	struct dlist *devlist = NULL;
 
-	/* cpufreqdev should have at least one child: the governor directory containing the parameter attributes */
-	devlist = sysfs_get_root_devices(cpufreqdev);
-	if (devlist == NULL) {
-		clog(LOG_ERR, "ERROR: no governor found in sysfs.\n");
-		return NULL;
-	}
-
-	/* Scan sub-devices for the governor (often only one to scan) */
-	dlist_for_each_data(devlist, govdev, struct sysfs_device) {
-		if (!strcmp(govdev->name, governor)) {
-			govattr = sysfs_get_device_attr(govdev, parameter);
-			if (govattr == NULL) {
-				clog(LOG_WARNING, "warning: %s governor does not "
-						"support parameter %s.\n", governor,
-						parameter);
-				return NULL;
-			}
-			return govattr;
-		}
-	}
-	clog(LOG_ERR, "ERROR: %s governor not found on sysfs\n", governor);
-	return NULL;
-}
-
-
-/* Opens a sysfs root device for the cpufreq interface of a given CPU number.
- * Returns a pointer to the root device if successfull, NULL on error.
- */
-static struct sysfs_root_device *open_cpufreq_root_device(unsigned int cpu)
-{
-	char cpufreqdev_path[SYSFS_PATH_MAX];
-	struct sysfs_root_device *cpufreqdev = NULL;
-
-	/* Construct sysfs root device path string for governor of this cpu */
-	snprintf(cpufreqdev_path, SYSFS_PATH_MAX, "system/cpu/cpu%d/cpufreq", cpu);
-	clog(LOG_DEBUG, "sysfs path cpufreq device = %s\n", cpufreqdev_path);
+	/* Construct sysfs path string for governor device of this cpu */
+	snprintf(govdev_path, SYSFS_PATH_MAX, "%s/cpu%u/cpufreq/%s", syspath_cpu, cpu, governor);
+	clog(LOG_DEBUG, "sysfs path governor device = %s\n", govdev_path);
 
 	/* Open sysfs governor device */
-	cpufreqdev = sysfs_open_root_device(cpufreqdev_path);
-	if (cpufreqdev == NULL) {
-		clog(LOG_ERR, "ERROR: cpufreq interface not found in sysfs for cpu%d.\n", cpu);
+	govdev = sysfs_open_device_path(govdev_path);
+	if (govdev == NULL) {
+		clog(LOG_ERR, "ERROR: device for governor %s not found in sysfs for cpu%u.\n", governor, cpu);
 		return NULL;
 	}
-	return cpufreqdev;
+	return govdev;
 }
 
 
-/* Reads the value of 'parameter' of the currently active governor on
- * sysfs root device 'cpufreqdev'.
- * Return -1 on error, or the parameter value otherwise.
+/* Reads the value of 'parameter' of the active governor
+ * Returns the paramter value, or -1 on error.
  */
-static long int get_parameter(struct sysfs_root_device *cpufreqdev, const char *governor,
-		const char *parameter)
+static long int get_parameter(struct sysfs_device *govdev, const char *parameter)
 {
 	struct sysfs_attribute *govattr = NULL;
 
-	govattr = get_governor_attribute(cpufreqdev, governor, parameter);
+	govattr = sysfs_get_device_attr(govdev, parameter);
 	if (govattr == NULL) {
-		clog(LOG_ERR, "ERROR: sysfs attribute %s could not be read for %s governor\n", parameter, governor);
+		clog(LOG_WARNING, "warning: attribute %s not found in sysfs.\n", parameter);
 		return -1;
 	}
 	return atol(govattr->value);
@@ -131,14 +95,14 @@ static long int get_parameter(struct sysfs_root_device *cpufreqdev, const char *
 static void set_parameter(const char *governor, const char *parameter, long int value, int is_percentage)
 {
 	unsigned int cpu;
-	char value_str[MAXLEN_PARAMETER_VALUE];
-	struct sysfs_root_device *cpufreqdev = NULL;
+	char value_str[BUFLEN_PARAMETER_VALUE];
+	struct sysfs_device *govdev = NULL;
 	struct sysfs_attribute *govattr = NULL;
 
 	/* For each CPU */
 	for (cpu = 0; cpu < number_of_cpus; ++cpu) {
-		cpufreqdev = open_cpufreq_root_device(cpu);
-		if (cpufreqdev == NULL) {
+		govdev = open_governor_device(cpu, governor);
+		if (govdev == NULL) {
 			return;
 		}
 		if (is_percentage) {
@@ -149,28 +113,55 @@ static void set_parameter(const char *governor, const char *parameter, long int 
 
 			/* Read minimum allowed value */
 			snprintf(min_name, SYSFS_NAME_LEN, "%s_min", parameter);
-			min = get_parameter(cpufreqdev, governor, min_name);
-			clog(LOG_DEBUG, "minimal value for %s: %ld\n", parameter, min);
+			min = get_parameter(govdev, min_name);
+			if (min < 0) {
+				sysfs_close_device(govdev);
+				clog(LOG_WARNING, "warning: minimum value for %s could not be read: ignored.\n", parameter);
+				return;
+			}
+			clog(LOG_DEBUG, "minimum value for %s: %ld\n", parameter, min);
 
 			/* Read maximum allowed value */
 			snprintf(max_name, SYSFS_NAME_LEN, "%s_max", parameter);
-			max = get_parameter(cpufreqdev, governor, max_name);
-			clog(LOG_DEBUG, "maximal value for %s: %ld\n", parameter, max);
+			max = get_parameter(govdev, max_name);
+			if (max < 0) {
+				sysfs_close_device(govdev);
+				clog(LOG_WARNING, "warning: maximum value for %s could not be read: ignored.\n", parameter);
+				return;
+			}
+			clog(LOG_DEBUG, "maximum value for %s: %ld\n", parameter, max);
 
 			/* Convert percentage to absolute value in a string */
 			abs_value = (long long int)value * (max - min) / 100 + min;
-			snprintf(value_str, MAXLEN_PARAMETER_VALUE, "%lu", (long int)abs_value);
+			snprintf(value_str, BUFLEN_PARAMETER_VALUE, "%lu", (long int)abs_value);
 			clog(LOG_DEBUG, "converted percentage %ld to absolute value: %s\n", value, value_str);
 		} else {
 			/* Convert value to string */
-			snprintf(value_str, MAXLEN_PARAMETER_VALUE, "%lu", value);
+			snprintf(value_str, BUFLEN_PARAMETER_VALUE, "%lu", value);
+		}
+
+		/* Get sysfs attribute for the parameter */
+		govattr = sysfs_get_device_attr(govdev, parameter);
+
+		/* Kernel compatibility issue: 
+		 * The parameter name "ignore_nice" changed to "ignore_nice_load" in kernel >= 2.6.16.
+		 * We are accepting both names silently, regardless the kernel version by trying to get
+		 * a sysfs device for the other name when the specified name doesn't exist in sysfs.
+		 */
+		if (govattr == NULL) {
+			if (strcmp(parameter, "ignore_nice") == 0) {
+				govattr = sysfs_get_device_attr(govdev, "ignore_nice_load");
+			} else if (strcmp(parameter, "ignore_nice_load") == 0) {
+				govattr = sysfs_get_device_attr(govdev, "ignore_nice");
+			}
+		}			
+		if (govattr == NULL) {
+			sysfs_close_device(govdev);
+			clog(LOG_WARNING, "warning: attribute %s not found in sysfs.\n", parameter);
+			return;
 		}
 
 		/* Write new value to sysfs' parameter attribute */
-		govattr = get_governor_attribute(cpufreqdev, governor, parameter);
-		if (govattr == NULL) {
-			return;
-		}
 		if (sysfs_write_attribute(govattr, value_str, strlen(value_str)) < 0) {
 			clog(LOG_ERR, "ERROR: could not set parameter %s to %s for %s governor on cpu%u: %s\n",
 					parameter, value_str, governor, cpu, strerror(errno));
@@ -178,8 +169,8 @@ static void set_parameter(const char *governor, const char *parameter, long int 
 		clog(LOG_DEBUG, "parameter %s set to %s for %s governor on cpu%u\n",
 				parameter, value_str, governor, cpu);
 
-		/* Close root device. This will free all structures allocated by libsysfs */
-		sysfs_close_root_device(cpufreqdev);
+		/* Close governor device. This will free all structures allocated by libsysfs */
+		sysfs_close_device(govdev);
 	}
 }
 
@@ -189,18 +180,19 @@ static void set_parameter(const char *governor, const char *parameter, long int 
 static int parameter_parse(const char *keyword, const char *value, void **obj, 
 		int accept_percentage, int accept_suffix)
 {
-	int i;
+	int i, j;
 	long int val;
 	int suffix_parsed;
 	int multiplier;
-	char strvalue[MAXLEN_PARAMETER_VALUE];
+	char suffix;
+	char strvalue[BUFLEN_PARAMETER_VALUE];
 	char *convcheck;
 	struct gov_parameter *ret;
 
 	clog(LOG_DEBUG, "called: %s = %s\n", keyword, value);
 	ret = malloc(sizeof(struct gov_parameter));
 	if (ret == NULL) {
-		clog(LOG_ERR, "ERROR: unable to make room for a governor parameter\n");
+		clog(LOG_ERR, "ERROR: not enough memory for a governor parameter\n");
 		return -1;
 	}
 
@@ -215,76 +207,89 @@ static int parameter_parse(const char *keyword, const char *value, void **obj,
 	snprintf(ret->parameter_name, SYSFS_NAME_LEN, "%s", keyword);
 
 	/* Make writable copy of the value string */
-	snprintf(strvalue, MAXLEN_PARAMETER_VALUE, "%s", value);
+	snprintf(strvalue, BUFLEN_PARAMETER_VALUE, "%s", value);
 
+	/* Parse suffix */
 	multiplier = 1;
 	ret->is_percentage = FALSE;
-	if (accept_percentage || accept_suffix) {
-		suffix_parsed = 0;
-		i = strlen(value) - 1;
-		if (accept_percentage && strvalue[i] == '%') {
-			ret->is_percentage = TRUE;
-			suffix_parsed = 1;
+	suffix = '\0';
+
+	/* Find where the number ends */
+	i = strspn(strvalue, "0123456789");
+	if (i == 0) {
+		clog(LOG_WARNING, "invalid number: %s=%s\n", keyword, value);
+		free(ret);
+		return -1;
+	}
+	j = i;
+
+	/* Skip whitespace */
+	while (strvalue[i] != '\0' && (strvalue[i] == ' ' || strvalue[i] == '\t')) {
+		++i;
+	}
+
+	if (strvalue[i] != '\0') { /* some suffix was specified */
+		if (!accept_percentage && !accept_suffix) {
+			clog(LOG_WARNING, "warning: no suffix allowed for %s\n", keyword);
+			free(ret);
+			return -1;
 		}
+		suffix = strvalue[i];
+		strvalue[j] = '\0';    /* cut suffix from the string to get clean number */
+		if (strvalue[++i] != '\0') {
+			free(ret);
+			clog(LOG_WARNING, "warning: only one-char suffix allowed: %s=%s\n", keyword, value);
+			return -1;
+		}
+		suffix_parsed = FALSE;
+		if (accept_percentage) {
+			if (suffix == '%') {
+				ret->is_percentage = TRUE;
+				suffix_parsed = TRUE;
+			}
+		} 
 		if (accept_suffix) {
-			switch (strvalue[i]) {
+			switch (suffix) {
 				case 'u':
 					multiplier = 1;
-					suffix_parsed = 1;
+					suffix_parsed = TRUE;
 					break;
 				case 'm':
 					multiplier = 1000;
-					suffix_parsed = 1;
+					suffix_parsed = TRUE;
 					break;
 				case 's':
 					multiplier = 1000000;
-					suffix_parsed = 1;
-					break;
+					suffix_parsed = TRUE;
 			}
 		}
-
-		/* Remove extra char from the string if needed */
-		if (suffix_parsed) {
-			do {
-				--i;
-			} while (i > 0 && (strvalue[i] == ' ' || strvalue[i] == '\t'));
-			if (i < 0) {
-				clog(LOG_WARNING, "governor parameter %s has no (percentage) value\n", keyword);
-				free(ret);
-				return -1;
-			}
-			strvalue[i + 1] = '\0';
+		if (!suffix_parsed) {
+			clog(LOG_WARNING, "warning: invalid suffix: %s=%s\n", keyword, value);
+			free(ret);
+			return -1;
 		}
 	}
 
 	/* Convert string to long int and check if it's a valid number */
 	val = strtol(strvalue, &convcheck, 10);
 	if (val == LONG_MAX) {
-		clog(LOG_WARNING, "governor parameter out of range %s=%s\n", keyword, strvalue);
+		clog(LOG_WARNING, "governor parameter out of range: %s = %s\n", keyword, strvalue);
 		free(ret);
 		return -1;
 	}
 	if (*convcheck != '\0') {
-		clog(LOG_WARNING, "governor parameter value invalid %s=%s\n", keyword, strvalue);
-		free(ret);
-		return -1;
-	}
-	if (val < 0) {
-		clog(LOG_WARNING, "governor parameter %s: negative numbers not allowed\n", keyword);
+		clog(LOG_WARNING, "governor parameter value invalid: %s = %s\n", keyword, strvalue);
 		free(ret);
 		return -1;
 	}
 	if (ret->is_percentage && val > 100) {
-		clog(LOG_WARNING, "governor parameter %s: percentage greater than 100%% not allowed\n", keyword);
+		clog(LOG_WARNING, "percentage greater than 100%% not allowed: %s=%s\n", keyword, strvalue);
 		free(ret);
 		return -1;
 	}
 
-	/* Apply multiplier from suffix if applicable */
-	if (suffix_parsed) {
-		val *=multiplier;
-	}
-
+   	/* Finish the object */
+	val *= multiplier;
 	ret->parameter_value = val;
 	*obj = ret;
 	return 0;
@@ -360,7 +365,7 @@ static void gov_parameter_post_change(void *obj, const struct cpufreq_policy *no
 		}
 		++i;
 	}
-	clog(LOG_INFO, "governor parameter %s specified, but %s does not support parameters\n", 
+	clog(LOG_INFO, "governor parameter %s specified, but %s is not supported by this plugin\n", 
 			parameter, new_policy->governor);
 }
 
@@ -369,11 +374,23 @@ static void gov_parameter_post_change(void *obj, const struct cpufreq_policy *no
  */
 static int gov_parameter_init(void)
 {
+	char sysfsmountpath[SYSFS_PATH_MAX];
 	struct cpufreqd_info const *info;
 
 	clog(LOG_DEBUG, "called\n");
+
+	/* Get number of CPU's */
 	info = get_cpufreqd_info();
 	number_of_cpus = info->cpus;
+
+	/* Construct sysfs base directory string for cpu device */
+	if (sysfs_get_mnt_path(sysfsmountpath, SYSFS_PATH_MAX) < 0) {
+		clog(LOG_ERR, "ERROR in call to libsysfs. Should not happen. Must be a bug..\n");
+		return -1;
+	}
+	clog(LOG_DEBUG, "sysfs mount path = %s\n", sysfsmountpath);
+	snprintf(syspath_cpu, SYSFS_PATH_MAX, "%s/%s", sysfsmountpath, SYS_CPU_DIR);
+	clog(LOG_DEBUG, "sysfs path cpu device  = %s\n", syspath_cpu);
 	return 0;
 }
 
